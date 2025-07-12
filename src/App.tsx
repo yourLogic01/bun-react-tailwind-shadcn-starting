731e1b11a3110c1141eb70e7d129349f4a347562
Salmansha08 <salmanwiharja8@gmail.com>
2025-07-12 22:50:55 +0700

    refactor: replace index.css with globals.css for improved styling structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Card, CardContent } from "@/components/ui/card";
 import { APITester } from "./APITester";
-import "./index.css";
+import "@/styles/globals.css";
 
 import logo from "./logo.svg";
 import reactLogo from "./react.svg";
