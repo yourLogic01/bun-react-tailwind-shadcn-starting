b46fdb2e0c4d721533ee27e79347529979d7b0e3
Salmansha08 <salmanwiharja8@gmail.com>
2025-07-14 22:07:12 +0700

    refactor: update import path for globals.css to relative path

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Card, CardContent } from "@/components/ui/card";
 import { APITester } from "./APITester";
-import "@/styles/globals.css";
+import "../styles/globals.css";
 
 import logo from "@/public/images/logo.svg";
 import reactLogo from "@/public/images/react.svg";
